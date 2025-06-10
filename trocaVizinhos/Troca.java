d82fed52bdd2bf2ec3706c8e13b3242f9d38c815
Lucas-Cunhaa <lucasgalvao260@gmail.com>
2025-06-10 15:33:07 -0300

    feat: add the trocaVizinho analises

--- trocaVizinhos/Troca.java
+++ trocaVizinhos/Troca.java
@@ -1,16 +1,26 @@
 import java.util.Arrays;
+import java.io.BufferedReader; // Importa a classe BufferedReader
+import java.io.IOException;    // Importa a classe IOException (para lidar com erros de I/O)
+import java.io.InputStreamReader; // Importa InputStreamReader
 
 public class Troca {
 
   public static void main(String[] args) {
     
-    int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    String[] nums = null; 
     
+    try {
+         String line = reader.readLine();
+         nums = line.split(" "); 
+
+    } catch (IOException ioe) {}
 
     int limit = nums.length % 2 == 0 ? nums.length : nums.length -1; 
 
     for (int i = 0; i < limit -1; i+=2) {
-        int temp = nums[i]; 
+        String temp = nums[i]; 
         
         nums[i] = nums[i+1]; 
         nums[i+1] = temp;
